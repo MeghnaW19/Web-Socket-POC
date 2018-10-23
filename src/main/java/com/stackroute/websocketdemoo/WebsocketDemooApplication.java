60de68496432ce06a8f26036b88cd0eb02dce631
Meghna <meghna@stackroute.in>
2018-10-23 10:09:25 +0530

    websocketdemo

---
+++ src/main/java/com/stackroute/websocketdemoo/WebsocketDemooApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.websocketdemoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketDemooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketDemooApplication.class, args);
+	}
+}
